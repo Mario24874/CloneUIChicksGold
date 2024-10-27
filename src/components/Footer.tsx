82e5e6a5447fb2250f1b75ab0b954998f03a84fe
Mario Moreno <marioivanmorenopineda@gmail.com>
2024-10-27 13:28:12 -0400

    Correccion Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import { Facebook, Instagram, Twitter, MessageCircle } from 'lucide-react';
 import '../styles/footer.css'; 
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
-      
-          <div className="social-links">
-            <a href="#" className="social-link"><Facebook className="social-icon" /></a>
-            <a href="#" className="social-link"><Instagram className="social-icon" /></a>
-            <a href="#" className="social-link"><Twitter className="social-icon" /></a>
-            <a href="#" className="social-link"><MessageCircle className="social-icon" /></a>
-          </div>          
-        </div>
+      <div className="payment-methods">
+        <img src="/assets/Visa.png" alt="Visa" className="payment-icon" />
+        <img src="/assets/MasterCard.png" alt="MasterCard" className="payment-icon" />
+        <img src="/assets/AmericanExpress.png" alt="American Express" className="payment-icon" />
+        <img src="/assets/Skrill.png" alt="Skrill" className="payment-icon" />
+        <img src="/assets/Cripto.png" alt="Cripto" className="payment-icon" />
+        <span className="more-text">and 50+ more</span>
+      </div>
 
-        <div className="footer-bottom">
+      <div className="container">
+        <div className="social-links">
+          <a href="#" className="social-link">
+            <img src="/assets/facebook.png" alt="Facebook" className="social-icon" />
+          </a>
+          <a href="#" className="social-link">
+            <img src="/assets/instagram.png" alt="Instagram" className="social-icon" />
+          </a>
+          <a href="#" className="social-link">
+            <img src="/assets/twitter.png" alt="Twitter" className="social-icon" />
+          </a>
+          <a href="#" className="social-link">
+            <img src="/assets/discord.png" alt="Discord" className="social-icon" />
+          </a>
+        </div>          
+      </div>
+
+      <div className="footer-bottom">
         <div className="footer-content">
           <div className="footer-section">
-          <img src="/assets/ChicksGold.png" alt="Chicks Gold" className="chicks-image" />
+            <img src="/assets/ChicksGold.png" alt="Chicks Gold" className="chicks-image" />
             <p className="footer-text">support@chicksgold.com</p>
           </div>
           
@@ -46,15 +61,17 @@ const Footer = () => {
               <li><a href="#" className="footer-link">Privacy Policy</a></li>
               <li><a href="#" className="footer-link">Terms of Service</a></li>
               <li><a href="#" className="footer-link">Copyright Policy</a></li>
-            </ul>            
+            </ul>
+            <div className="trustpilot-section">
+              <img src="/assets/starsTrust.png" alt="Stars Trust" className="stars-trust-icon" />
+              <p className="trustpilot-text">Trustpilot Reviews</p>
+            </div>
           </div>
         </div>
 
         <div className="footer-copyrigth">
           <p className="copyright-text">© 2017 - 2024 Chicksgold.com. All Rights Reserved</p>
         </div>
-        
-        
       </div>
     </footer>
   );
